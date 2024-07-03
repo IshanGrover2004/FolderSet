cb83d5d2e4aabac19f5875a9fa017490b34f15cd
IshanGrover2004 <groverishan2004@gmail.com>
2024-07-03 21:28:39 +0530

    feat: Add handling functions for root, sigin & signup endpoints

---
+++ src/handler.rs
@@ -0,0 +1,51 @@
+use actix_web::{get, post, web, HttpResponse, Responder};
+use bcrypt::verify;
+
+use crate::{AppState, SigninRequest, SignupRequest};
+
+#[get("/")]
+async fn handle_root() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[post("/signup")]
+async fn handle_signup(
+    request: web::Json<SignupRequest>,
+    data: web::Data<AppState>,
+) -> impl Responder {
+    let mut app_data = data.users.lock().expect("Unable to access the DB");
+
+    let hashed_password =
+        bcrypt::hash(request.password.clone(), 7).expect("Unable to hash user password");
+
+    if app_data.contains_key(&request.email) {
+        HttpResponse::Conflict().body("Username already exists")
+    } else {
+        app_data.insert(request.email.clone(), hashed_password);
+        println!("SignedUp successfully");
+        HttpResponse::Ok().body("User created successfully")
+    }
+}
+
+#[post("/signin")]
+async fn handle_signin(
+    request: web::Json<SigninRequest>,
+    data: web::Data<AppState>,
+) -> impl Responder {
+    let app_data = data.users.lock().expect("Unable to access the DB");
+    if let Some(hashed_password) = app_data.get(&request.email) {
+        if verify(request.password.clone(), hashed_password)
+            .expect("Unable to compare hashed password")
+        {
+            println!("SignedIn successfully");
+
+            // Generate JWT key here & send as response
+
+            HttpResponse::Ok().body("Successfully Signed in")
+        } else {
+            HttpResponse::Unauthorized().body("Invalid Credentials")
+        }
+    } else {
+        HttpResponse::Unauthorized().body("Invalid Credentials")
+    }
+}
