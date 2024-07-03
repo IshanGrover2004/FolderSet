d4e3b5d0518bff0dda7d7f55bbf467f1019102d1
IshanGrover2004 <groverishan2004@gmail.com>
2024-07-03 21:27:59 +0530

    feat: Add structs for signin & signup

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,22 @@
+use serde::{Deserialize, Serialize};
+
 pub mod handler;
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct SignupRequest {
+    pub name: String,
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct SigninRequest {
+    pub email: String,
+    pub password: String,
+}
+
+// In-memory storage
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AppState {
+    pub users: std::sync::Mutex<std::collections::HashMap<String, String>>,
+}
