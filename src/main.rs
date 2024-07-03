86cd87edf4de5e828841f3766e3bc914e9640b32
IshanGrover2004 <groverishan2004@gmail.com>
2024-07-03 21:29:00 +0530

    feat: Add main function to run actix web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+use actix_web::middleware::Logger;
+use actix_web::{web, App, HttpServer};
+use folderset::handler::{handle_root, handle_signin, handle_signup};
+use folderset::AppState;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "actix_web=info");
+    }
+
+    env_logger::init();
+
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
+    println!("🚀 Server started successfully on {}", addr);
+
+    let app_state = web::Data::new(AppState {
+        users: std::sync::Mutex::new(std::collections::HashMap::new()),
+    });
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(app_state.clone())
+            .service(handle_root)
+            .service(handle_signup)
+            .service(handle_signin)
+            .wrap(Logger::default())
+    })
+    .bind(addr)?
+    .run()
+    .await
 }
