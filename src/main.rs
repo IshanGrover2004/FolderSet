4bfb127828b76a88a4346f69198ff3c275d07c59
Piyush <91911367+piyushmishra1416@users.noreply.github.com.>
2024-07-09 15:52:32 +0530

    updated the main function

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use folderset::handler::{handle_root, handle_signin, handle_signup};
+use sqlx::postgres::PgPoolOptions;
 use folderset::AppState;
 
 #[actix_web::main]
@@ -13,12 +14,17 @@ async fn main() -> std::io::Result<()> {
 
     env_logger::init();
 
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://myuser:mysecretpassword@localhost/mydatabase")
+        .await
+        .expect("Unable to connect to the database");
+
     let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
     println!("🚀 Server started successfully on {}", addr);
 
-    let app_state = web::Data::new(AppState {
-        users: std::sync::Mutex::new(std::collections::HashMap::new()),
-    });
+    
+    let app_state = web::Data::new(AppState { pool });
 
     HttpServer::new(move || {
         App::new()
